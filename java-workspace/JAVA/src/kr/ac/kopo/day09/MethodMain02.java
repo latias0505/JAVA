a6fe5eb4148b80d28bb54046943754f51802cb3d
Kang Sunghyuk <latias0505@naver.com>
2023-03-17 16:03:23 +0900

    day09 내용 수정

--- java-workspace/JAVA/src/kr/ac/kopo/day09/MethodMain02.java
+++ java-workspace/JAVA/src/kr/ac/kopo/day09/MethodMain02.java
@@ -5,7 +5,10 @@ public class MethodMain02 {
 	public static void main(String[] args) {
 		Method m = new Method(); // 다른 클래스에 있는 메소드를 호출하기 위한 함수
 		m.call(); // 함수로 다른 클래스에 있는 메소드를 호출하기
-		m.call(10);
+		m.call(10); //Method 클래스에 있는 call(int i)에 10을 전송함
+		m.call("aaa", 12);
+		m.call('a');
+		m.call(12.34);
 
 	}
 
