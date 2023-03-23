a3e14fee3bd3fe14cbafb3b01f4aa207cb18a04b
Kang Sunghyuk <latias0505@naver.com>
2023-03-23 19:18:20 +0900

    다이어그램 테스트 업로드

---
+++ java-workspace/JAVA/src/kr/ac/kopo/test/Manager.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo.test;
+
+public class Manager extends kr.ac.kopo.test.Employee {
+    private Employee[] empList;
+
+    public Manager(){
+    }
+
+    public Manager(int no, String name, int salary, String grade, Employee[] empList){
+    }
+
+    public void info(){
+    }
+
+}
